bf98952d091fcb3d66ee4ee5490af8144e88f70d
Jaiki Sharma <jack912062sharmas@gmail.com>
2025-12-18 01:01:54 +0530

    CSS styling

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const mongoose = require("mongoose");
 const Listing = require("./models/listing.js");
 const path = require("path");
 const methodOverride = require("method-override");
+const ejsMate = require("ejs-mate");
 
 const MONGO_URL = "mongodb://127.0.0.1:27017/wanderlust";
 
@@ -21,6 +22,8 @@ app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
 app.use(express.urlencoded({extended:true}));
 app.use(methodOverride("_method"));
+app.engine('ejs', ejsMate);
+app.use(express.static(path.join(__dirname,"public")));
 
 app.get("/",(req,res)=>{
     res.send("Hi i am here");
@@ -39,25 +42,48 @@ app.get("/listings/new" ,(req,res)=>{
 })
 
 //Show Route
-app.get("/listings/:id" , async(req,res)=>{
-    let {id} = req.params;
+
+app.get("/listings/:id", async (req, res) => {
+    const { id } = req.params;
+    if (!mongoose.Types.ObjectId.isValid(id)) {
+        return res.status(404).send("Listing not found");
+    }
+
     const listing = await Listing.findById(id);
-    res.render("listings/show.ejs", {listing});
+
+    if (!listing) {
+        return res.status(404).send("Listing not found");
+    }
+
+    res.render("listings/show.ejs", { listing });
 });
+
 
 //Create Route
 app.post("/listings", async(req,res)=>{
     const newListing = new Listing(req.body.listing);
+    // console.log(req.body.listing);
     await newListing.save();
     res.redirect("/listings");
 });
 
 //Edit Route
-app.get("/listings/:id/edit" ,async(req,res)=>{
-     let {id} = req.params;
+
+app.get("/listings/:id/edit", async (req, res) => {
+    const { id } = req.params;
+    if (!mongoose.Types.ObjectId.isValid(id)) {
+        return res.status(404).send("Invalid listing ID");
+    }
+
     const listing = await Listing.findById(id);
-    res.render("listings/edit.ejs", {listing});
+
+    if (!listing) {
+        return res.status(404).send("Listing not found");
+    }
+
+    res.render("listings/edit.ejs", { listing });
 });
+
 
 //update Route
 app.put("/listings/:id",async(req,res)=>{

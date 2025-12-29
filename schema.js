122889cdcd2ccc270fc0a4135b0d359328146857
Jaiki Sharma <jack912062sharmas@gmail.com>
2025-12-29 22:32:06 +0530

    schema new

--- schema.js
+++ schema.js
@@ -6,7 +6,6 @@ module.exports.listingSchema = Joi.object({
         description : Joi.string().required(),
         country : Joi.string().required(),
         location : Joi.string().required(),
-        price : Joi.number().required().min(0),
-        image : Joi.string().allow("",null)
+        price : Joi.number().required().min(0)
     }).required()
 })

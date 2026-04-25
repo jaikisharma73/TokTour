bdef7b6badc2d5094fdf7398080375c587ef071d
Jaiki Sharma <jack912062sharmas@gmail.com>
2026-04-25 12:06:18 +0530

    signup done

---
+++ routes/user.js
@@ -0,0 +1,26 @@
+const express = require("express");
+const router = express.Router();
+const User = require("../models/user.js");
+const wrapAsync = require("../utils/wrapAsync.js");
+
+router.get("/signup", (req, res) => {
+    res.render("users/signup");
+});
+
+router.post("/signup", wrapAsync(async (req, res) => {
+    try {
+        let { username, email, password } = req.body;
+        const newUser = new User({ email, username });
+        const registeredUser = await User.register(newUser, password);
+        console.log(registeredUser);
+        req.flash("success", "You are registered now");
+        res.redirect("/listings");
+    } catch (err) {
+        req.flash("error", err.message);
+        res.redirect("/signup");
+    }
+
+}));
+
+
+module.exports = router;

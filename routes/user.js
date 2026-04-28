8875b6956005766b460bf5095b65f42cef2fa06b
Jaiki Sharma <jack912062sharmas@gmail.com>
2026-04-28 20:00:41 +0530

    logOut done

--- routes/user.js
+++ routes/user.js
@@ -14,8 +14,13 @@ router.post("/signup", wrapAsync(async (req, res) => {
         const newUser = new User({ email, username });
         const registeredUser = await User.register(newUser, password);
         console.log(registeredUser);
-        req.flash("success", "You are registered now");
-        res.redirect("/listings");
+        req.login(registeredUser, (err) => {
+            if (err) {
+                return next(err);
+            }
+            req.flash("success", "Welcome to TokTour! You are registered now");
+            res.redirect("/listings");
+        })
     } catch (err) {
         req.flash("error", err.message);
         res.redirect("/signup");
@@ -30,5 +35,15 @@ router.post("/login", passport.authenticate("local", { failureRedirect: "/login"
     req.flash("success", "Welcome To TokTour");
     res.redirect("/listings");
 });
+
+router.get("/logout", (req, res, next) => {
+    req.logout((err) => {
+        if (err) {
+            return next(err);
+        }
+        req.flash("success", "You are logged out");
+        res.redirect("/listings");
+    })
+})
 
 module.exports = router;

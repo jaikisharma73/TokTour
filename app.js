6a1b8beda3080c2eeb5a8077e8ec288eb567d347
Jaiki Sharma <jack912062sharmas@gmail.com>
2026-05-03 22:18:33 +0530

    navbar done

--- app.js
+++ app.js
@@ -51,9 +51,9 @@ const sessionOptions = {
     }
 };
 
-app.get("/", (req, res) => {
-    res.send("Hi i am here");
-})
+// app.get("/", (req, res) => {
+// //     res.send("Hi i am here");
+// // })
 
 app.use(session(sessionOptions));
 app.use(flash());

cd337ad9455021323286264e11181e0999e16919
Jaiki Sharma <jack912062sharmas@gmail.com>
2026-05-04 14:29:39 +0530

    donee alll

--- routes/listing.js
+++ routes/listing.js
@@ -4,7 +4,7 @@ const wrapAsync = require("../utils/wrapAsync.js");
 const { isLoggedIn, isOwner, validateListing } = require("../middleware.js")
 const listingController = require("../controllers/listings.js");
 const multer = require('multer')
-const { storage } = require("../cloudCOnfig.js");
+const { storage } = require("../cloudConfig.js");
 
 
 const upload = multer({ storage });

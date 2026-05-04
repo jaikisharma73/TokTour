80be4ed8146136d80e532178a16c09dd95a59949
Jaiki Sharma <jack912062sharmas@gmail.com>
2026-05-04 13:20:25 +0530

    Error mongoose

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ if (process.env.NODE_ENV != "production") {
 const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
+require("mongoose/lib/connectionstate");
 const path = require("path");
 const methodOverride = require("method-override");
 const ejsMate = require("ejs-mate");
